b16f22364523e98dfad90bd367027df1e756bbf5
mikelarao <michaelangelo.arao@gmail.com>
2016-09-28 15:43:44 +0800

    updated view

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	sass = require('gulp-ruby-sass'),
-	plumber = require('gulp-plumber'),
-	browserSync = require('browser-sync'),
-	rename = require("gulp-rename"),
-	sourcemaps = require('gulp-sourcemaps');
+uglify = require('gulp-uglify'),
+sass = require('gulp-ruby-sass'),
+plumber = require('gulp-plumber'),
+browserSync = require('browser-sync'),
+rename = require("gulp-rename"),
+sourcemaps = require('gulp-sourcemaps');
+autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('scripts', function() {
 	gulp.src('js/*.js')
@@ -12,34 +13,40 @@ gulp.task('scripts', function() {
 	.pipe(uglify())
 	.pipe(rename({
 		suffix: '.min'
-    }))
+	}))
 	.pipe(gulp.dest('app/assets/js/'))
 });
 
 gulp.task('styles', function() {
 	return sass('sass/*.sass', { sourcemap: true, style: 'expanded' })
-    .on('error', sass.logError)
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('app/assets/css/'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+	.on('error', sass.logError)
+	.pipe(autoprefixer())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('app/assets/css/'))
+	.pipe(browserSync.reload({
+		stream: true
+	}))
 });
 
-
 gulp.task('browserSync', function() {
-  browserSync({
-    server: {
-      baseDir: './app/'
-    },
-  })
+	browserSync({
+		server: {
+			baseDir: './app/'
+		},
+	})
+});
+
+// Copy all vendors to build folder
+gulp.task('vendors', function () {
+	gulp.src(['vendors/**/*']).pipe(gulp.dest('app/vendors/'));
 });
 
 gulp.task('watch', ['browserSync'], function() {
 	gulp.watch('js/*.js', ['scripts']);
 	gulp.watch('sass/**/*', ['styles']);
+	gulp.watch('vendors/**/*', ['vendors']);
 	gulp.watch('app/*.html').on('change', browserSync.reload);
 	gulp.watch('app/assets/js/main.js').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['scripts', 'vendors', 'styles', 'watch']);
